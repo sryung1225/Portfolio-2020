aba15e62651acaa741296c3a89b07d67d837778c
sryung1225 <sryung1225@gmail.com>
2020-12-10 08:22:18 +0900

    works layout - tabmenu

---
+++ port/assets/js/custom.js
@@ -0,0 +1,16 @@
+/* 탭 메뉴 */
+var tabBtn = $(".tab-box > .tab-btn > ul > li");
+var tabCont = $(".tab-box > .tab-cont > div");
+
+tabCont.hide().eq(0).show();
+
+tabBtn.click(function(e){
+    e.preventDefault();
+    var target = $(this);
+    var index = target.index();
+    /* alert(index); */
+    tabBtn.removeClass("active");
+    target.addClass("active");
+    tabCont.css("display","none");
+    tabCont.eq(index).css("display","block");
+});
